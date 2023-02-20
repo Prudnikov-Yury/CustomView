1aa04fd9231a678064c60233f9e3ad36afa0c87c
Yury Prudnikov <struck.yury@gmail.com>
2023-02-20 10:24:04 +0300

    Add custom bottom buttons

--- app/src/main/java/com/pr/customview/BottomButtonsView.kt
+++ app/src/main/java/com/pr/customview/BottomButtonsView.kt
@@ -3,11 +3,19 @@ package com.pr.customview
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import com.pr.customview.databinding.PartButtonsBinding
+
+enum class BottomButtonAction {
+    POSITIVE, NEGATIVE
+}
+
+typealias OnBottomButtonsActionListener = (BottomButtonAction) -> Unit
 
 class BottomButtonsView(
     context: Context,
@@ -18,14 +26,35 @@ class BottomButtonsView(
 
     private val binding: PartButtonsBinding
 
+    private var listener: OnBottomButtonsActionListener? = null
+
+    var isProgressMode: Boolean = false
+        set(value) {
+            field = value
+            if (value) {
+                binding.positiveButton.isVisible = false
+                binding.negativeButton.isVisible = false
+                binding.progress.isVisible = true
+            } else {
+                binding.positiveButton.isVisible = true
+                binding.negativeButton.isVisible = true
+                binding.progress.isVisible = false
+            }
+        }
+
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : this(
         context,
         attrs,
         defStyleAttr,
-        0
+        R.style.MyBottomButtonsStyle
     )
 
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?) : this(
+        context,
+        attrs,
+        R.attr.bottomButtonsStyle
+    )
+
     constructor(context: Context) : this(context, null)
 
     init {
@@ -33,6 +62,7 @@ class BottomButtonsView(
         inflater.inflate(R.layout.part_buttons, this, true)
         binding = PartButtonsBinding.bind(this)
         initializeAttributes(attrs, defStyleAttr, defStyleRes)
+        initListeners()
     }
 
     private fun initializeAttributes(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
@@ -45,35 +75,101 @@ class BottomButtonsView(
         )
 
         with(binding) {
-            val positiveButtonText = typedArray.getString(R.styleable.BottomButtonsView_bottomPositiveButtonText)
-            binding.positiveButton.text = positiveButtonText ?: "Ok"
+            val positiveButtonText =
+                typedArray.getString(R.styleable.BottomButtonsView_bottomPositiveButtonText)
+            setPositiveButtonText(positiveButtonText)
 
-            val negativeButtonText = typedArray.getString(R.styleable.BottomButtonsView_bottomNegativeButtonText)
-            binding.negativeButton.text = negativeButtonText ?: "Cancel"
+            val negativeButtonText =
+                typedArray.getString(R.styleable.BottomButtonsView_bottomNegativeButtonText)
+            setNegativeButtonText(negativeButtonText)
 
-            val bottomPositiveBackgroundColor = typedArray.getColor(R.styleable.BottomButtonsView_bottomPositiveBackgroundColor, Color.BLACK)
-            binding.positiveButton.backgroundTintList = ColorStateList.valueOf(bottomPositiveBackgroundColor)
+            val bottomPositiveBackgroundColor = typedArray.getColor(
+                R.styleable.BottomButtonsView_bottomPositiveBackgroundColor,
+                Color.BLACK
+            )
+            binding.positiveButton.backgroundTintList =
+                ColorStateList.valueOf(bottomPositiveBackgroundColor)
 
             val bottomNegativeBackgroundColor = typedArray.getColor(
                 R.styleable.BottomButtonsView_bottomNegativeBackgroundColor,
                 Color.WHITE
             )
-            binding.negativeButton.backgroundTintList = ColorStateList.valueOf(bottomNegativeBackgroundColor)
+            binding.negativeButton.backgroundTintList =
+                ColorStateList.valueOf(bottomNegativeBackgroundColor)
 
-            val isProgressMode = typedArray.getBoolean(R.styleable.BottomButtonsView_bottomProgressMode, false)
-
-            if (isProgressMode) {
-                positiveButton.isVisible = false
-                negativeButton.isVisible = false
-                progress.isVisible = true
-            } else {
-                positiveButton.isVisible = true
-                negativeButton.isVisible = true
-                progress.isVisible = false
-            }
+            this@BottomButtonsView.isProgressMode =
+                typedArray.getBoolean(R.styleable.BottomButtonsView_bottomProgressMode, false)
         }
 
         typedArray.recycle()
     }
 
+    private fun initListeners() {
+        binding.positiveButton.setOnClickListener {
+            this.listener?.invoke(BottomButtonAction.POSITIVE)
+        }
+
+        binding.negativeButton.setOnClickListener {
+            this.listener?.invoke(BottomButtonAction.NEGATIVE)
+        }
+    }
+
+    fun setListener(listener: OnBottomButtonsActionListener?) {
+        this.listener = listener
+    }
+
+    fun setPositiveButtonText(text: String?) {
+        binding.positiveButton.text = text ?: "Ok"
+    }
+
+    fun setNegativeButtonText(text: String?) {
+        binding.negativeButton.text = text ?: "Cancel"
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState() ?: BaseSavedState.EMPTY_STATE
+        val savedState = SavedState(superState)
+        savedState.positiveButtonText = binding.positiveButton.text.toString()
+        savedState.negativeButtonText = binding.negativeButton.text.toString()
+        return savedState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        binding.positiveButton.text = savedState.positiveButtonText
+        binding.negativeButton.text = savedState.negativeButtonText
+    }
+
+    class SavedState : BaseSavedState {
+
+        var positiveButtonText: String? = null
+        var negativeButtonText: String? = null
+
+        constructor(superState: Parcelable): super(superState)
+        constructor(parcel: Parcel): super(parcel) {
+            positiveButtonText = parcel.readString()
+            negativeButtonText = parcel.readString()
+
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeString(positiveButtonText)
+            out.writeString(negativeButtonText)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState> = object: Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return  Array(size) { null }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/pr/customview/MainActivity.kt
+++ app/src/main/java/com/pr/customview/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.pr.customview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.pr.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
+
+        binding.bottomButtons.setListener { action ->
+
+            when (action) {
+                BottomButtonAction.POSITIVE -> {
+                    binding.bottomButtons.setPositiveButtonText("Updated Ok")
+                }
+                BottomButtonAction.NEGATIVE -> {
+                    binding.bottomButtons.setNegativeButtonText("Updated Cancel")
+                }
+            }
+        }
     }
+
 }
